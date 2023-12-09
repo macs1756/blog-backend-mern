e653807e0a9d0d7602594b90d2ebd3ead0f7754f
macs1756 <kindrativmaksum312@gmail.com>
2023-12-09 16:45:13 +0200

    v7

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
 
 
 //Register
@@ -7,9 +8,32 @@ export const register = async (req,res) => {
       
       const { username, password } = req.body
 
+      const isUsed = await User.findOne({username})
+
+      if(isUsed){
+        return res.status(402).json({
+          messange: 'Person with this username is used',
+        })
+      }
+
+      const salt = bcrypt.genSaltSync(10)
+      const hash = bcrypt.hashSync(password, sait)
+
+      const newUser = new User({
+        username,
+        password: hash
+      })
+
+      await newUser.save()
+
+      res.status(200).json({
+          messange: 'Person created'
+      })
+
     } catch (error) {
-      
+        console.log(error)
     }
+
 }
 
 //Login
