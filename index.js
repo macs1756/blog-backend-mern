df9a52b9642055119db9545cebe55ba3d9ae28a6
macs1756 <kindrativmaksum312@gmail.com>
2023-12-09 15:07:52 +0200

    v4

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 
-app.listen(5000, ()=>{
-      console.log('server is working...')
-})
+function startServer() {
+      app.listen(5000, ()=>{
+            console.log('server working ...')
+      })
+}
+
+async function connectToBase() {
+      try {
+
+
+            const databaseUri = 'mongodb+srv://macs1756:test12345@cluster1.jidxccl.mongodb.net/blog?retryWrites=true&w=majority';
+
+            await mongoose.connect(databaseUri);
+                  //mongoose.connect('mongodb://username:password@host:port/database?options...');
+            startServer();
+            
+      } catch (error) {
+            console.log(error)
+      }
+}
+
+
+connectToBase()
