e7bacd7959b8dd278c0cee75a61ca3fe79c6eac2
macs1756 <kindrativmaksum312@gmail.com>
2023-12-09 15:59:08 +0200

    v6

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
 const app = express()
+dotenv.config()
+
+//constans
+const PORT = process.env.PORT 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+//Middleware
+app.use(cors())
+app.use(express.json())
 
 function startServer() {
-      app.listen(5000, ()=>{
+
+      app.get('/', (req, res) =>{
+            res.json({mes: "is working"})
+      })
+
+      app.listen( PORT , ()=>{
             console.log('server working ...')
       })
 }
+
+
 
 async function connectToBase() {
       try {
 
 
-            const databaseUri = 'mongodb+srv://macs1756:test12345@cluster1.jidxccl.mongodb.net/blog?retryWrites=true&w=majority';
-
+            const databaseUri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster1.jidxccl.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
             await mongoose.connect(databaseUri);
       
             startServer();
