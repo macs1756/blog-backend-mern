5433d2be2c876dea85a1510a64dc3c4a06bf46e9
macs1756 <kindrativmaksum312@gmail.com>
2023-12-10 17:23:36 +0200

    v13

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,11 @@ import { checkAuth } from '../utils/checkAuth.js'
 
 const router = new Router()
 
-
 ///Register
 router.post('/register', register)
 
 ///Login
 router.post('/login', login)
-
 
 ///Get me
 router.get('/me',  checkAuth ,getMe)
