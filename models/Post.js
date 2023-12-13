34ae2c7f90b644aac9ecb945d426e6970dc02a4d
macs1756 <kindrativmaksum312@gmail.com>
2023-12-13 18:43:04 +0200

    v15

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,37 @@
-import 
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+    required: true,
+    default: ''
+  },
+
+  views: {
+    type: number,
+    default: 0,
+  },
+
+  autor: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'Comment'
+  }]
+
+}, { timestamps })
+
+export default mongoose.model('Post', postSchema)
