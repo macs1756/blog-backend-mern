a3d5660a4088ed615d918a47aecad7af622dc5a0
macs1756 <kindrativmaksum312@gmail.com>
2023-12-13 19:58:44 +0200

    v17

--- controllers/post.js
+++ controllers/post.js
@@ -1,19 +1,56 @@
 //controller
 import Post from '../models/Post.js'
 import User from '../models/User.js'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-export const createPost = async (req,res) => {
+export const createPost = async (req, res) => {
 
   try {
 
-    res.json({messange: "create post"})
+    const { title, description } = req.body
+    const user = await User.findById(req.userId)
+
+    if (req.files) {
+
+      let fileName = Date.now().toString() + req.files.image.name
+
+      const __dirname = dirname(fileURLToPath(import.meta.url))
+
+      req.files.image.mv(path.join(__dirname, '..', 'uploads', fileName))
+
+      const newPostWithImage = new Post({
+        username: user.username,
+        title,
+        description,
+        image: fileName,
+        autor: req.userId,
+      })
+
+      await newPostWithImage.save()
+
+      await User.findByIdAndUpdate(req.userId, {
+        $push: {
+          posts: newPostWithImage
+        }
+      })
+
+      res.json({ messange: newPostWithImage })
+
+    }
+
+    const newPostWothOutImage = new Post({
+      
+    })
+    
+
 
   } catch (error) {
     res.json({
       messange: 'Error on server'
     })
   }
-  
+
 
 }
 
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { createPost } from '../controllers/post.js'
+import checkAuth from '../utils/checkAuth.js'
 
 const router = new Router()
 
 ///Create
-router.post('/create', createPost)
+router.post('/create', checkAuth, createPost)
 
 
 export default router
