1411e389e4ee36d39f1d9643ac4d5d94352b3677
macs1756 <kindrativmaksum312@gmail.com>
2023-12-19 19:51:22 +0200

    final version endpoint create comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -8,18 +8,26 @@ export const createComment = async (req, res) => {
     const { postId, comment } = req.body
 
 
-    if (!comment) {
-      res.json({ messange: 'Not found body for comment' })
+    if (!comment) res.json({ messange: 'Not found body for comment' })
+    
+    const newComment = new Comment({ comment })
+    await newComment.save()
+
+
+    try {
+      await Post.findByIdAndUpdate(postId, {
+        $push: {comments:  newComment?._id}
+      })
+
+    } catch (error) {
+      res.json({error})
     }
 
-    res.json({
-      messange: 'Comments create'
-    })
+
+    res.json({ messange: newComment })
 
   } catch (error) {
-    res.status(400).json({
-      messange: 'Person uncreated'
-    })
+    res.status(400).json({ messange: 'Person uncreated' })
   }
 
 }
